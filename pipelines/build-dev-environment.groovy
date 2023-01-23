afd86f82b20deb2b5946f8989c16c0aefef0aef3
José Caetano Silva <caetanator@hotmail.com>
2023-01-23 18:04:11 +0000

    Issues fixed.

--- pipelines/build-dev-environment.groovy
+++ pipelines/build-dev-environment.groovy
@@ -8,16 +8,16 @@ pipeline {
     }
 
     parameters {
-        string name: 'ENVIRONMENT_NAME', trim: true     
-        password defaultValue: '', description: 'Password to use for MySQL container - root user', name: 'MYSQL_PASSWORD'
-        string name: 'MYSQL_PORT', trim: true  
+        string(name: 'ENVIRONMENT_NAME', trim: true)
+        password(name: 'MYSQL_PASSWORD', defaultValue: '', description: 'Password to use for MySQL container - root user')
+        string(name: 'MYSQL_PORT', trim: true)
 
         booleanParam(name: 'SKIP_STEP_1', defaultValue: false, description: 'STEP 1 - RE-CREATE DOCKER IMAGE')
     }
-  
+
     stages {
         stage('Checkout GIT repository') {
-            steps {     
+            steps {
               script {
                 git branch: 'master',
                 credentialsId: '21f01d09-06da9cc35103',
@@ -26,17 +26,17 @@ pipeline {
             }
         }
         stage('Create latest Docker image') {
-            steps {     
+            steps {
               script {
-                if (!params.SKIP_STEP_1){    
+                if (!params.SKIP_STEP_1){
                     echo "Creating docker image with name $params.ENVIRONMENT_NAME using port: $params.MYSQL_PORT"
-                    sh """
-                    sed 's/<PASSWORD>/$params.MYSQL_PASSWORD/g' pipelines/include/create_developer.template > pipelines/include/create_developer.sql
-                    """
+                    sh '''
+                    sed 's/<PASSWORD>/${MYSQL_PASSWORD}/g' pipelines/include/create_developer.template > pipelines/include/create_developer.sql
+                    '''
 
-                    sh """
-                    docker build pipelines/ -t $params.ENVIRONMENT_NAME:latest
-                    """
+                    sh '''
+                    docker build pipelines/ -t ${ENVIRONMENT_NAME}:latest
+                    '''
 
                 }else{
                     echo "Skipping STEP1"
@@ -45,18 +45,18 @@ pipeline {
             }
         }
         stage('Start new container using latest image and create user') {
-            steps {     
+            steps {
               script {
-                
+
                 def dateTime = (sh(script: "date +%Y%m%d%H%M%S", returnStdout: true).trim())
                 def containerName = "${params.ENVIRONMENT_NAME}_${dateTime}"
-                sh """
-                docker run -itd --name ${containerName} --rm -e MYSQL_ROOT_PASSWORD=$params.MYSQL_PASSWORD -p $params.MYSQL_PORT:3306 $params.ENVIRONMENT_NAME:latest
-                """
+                sh '''
+                docker run -itd --name ${containerName} --rm -e MYSQL_ROOT_PASSWORD="${MYSQL_PASSWORD}" -p ${MYSQL_PORT}:3306 ${ENVIRONMENT_NAME}:latest
+                '''
 
-                sh """
-                docker exec ${containerName} /bin/bash -c 'mysql --user="root" --password="$params.MYSQL_PASSWORD" < /scripts/create_developer.sql'
-                """
+                sh '''
+                docker exec ${containerName} /bin/bash -c 'mysql --user="root" --password="${MYSQL_PASSWORD}" < /scripts/create_developer.sql'
+                '''
 
                 echo "Docker container created: $containerName"
 
